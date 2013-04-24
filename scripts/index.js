060478eb43327bb19dd8660255c0efb987fea80b
Abraham Lapscher <ebirito@yahoo.com>
2013-04-23 23:44:23 -0400

    Login with credentials

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,16 @@
 function Login()
 {
-	$.mobile.changePage('pages/payment.html');
+	var user = new StackMob.User({ username: $("#txtEmail").val(), password: $("#txtPassword").val() });
+	//Makes a call to StackMob to request a login
+	user.login(false, {
+	  success: function(model) {
+	    $.mobile.changePage('pages/payment.html');
+	  }, 
+	  error: function(model, response) {
+	    var validator = $("#frmLogin").validate();
+		validator.showErrors({"txtPassword": "E-mail o clave no validos (punto@punto.com, 12345)"});
+	  }
+	});
 }
 
 function lnkTermsClick()
