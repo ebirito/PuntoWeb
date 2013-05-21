3a4fb741c837374152d2134e77915316ec700224
Abraham Lapscher <ebirito@yahoo.com>
2013-05-20 23:59:52 -0400

    Audio reader handler

--- scripts/payment.js
+++ scripts/payment.js
@@ -2,3 +2,25 @@ function btnPayClick()
 {
 	$.mobile.changePage('signature.html');
 }
+
+function CheckHeadset()
+{
+	HeadsetWatcher.watch(function(result) {
+	ToggleActions(result.plugged);
+  });
+}
+
+function ToggleActions(readerPlugged)
+{
+	$("#txtPaymentAmount").prop('disabled', !readerPlugged);
+	if (readerPlugged){
+		$("#divPay").show();
+		$("#divInsertReader").hide();
+		$("#txtPaymentAmount").focus();
+	}
+	else{
+		$("#divPay").hide();
+		$("#divInsertReader").show();
+	}
+	
+}
