df285230d630f2ac483f017a56708e6370716d65
Abraham Lapscher <ebirito@yahoo.com>
2013-05-22 19:03:29 -0400

    Revert headset change

--- scripts/payment.js
+++ scripts/payment.js
@@ -2,25 +2,3 @@ function btnPayClick()
 {
 	$.mobile.changePage('signature.html');
 }
-
-function CheckHeadset()
-{
-	HeadsetWatcher.watch(function(result) {
-	ToggleActions(result.plugged);
-  });
-}
-
-function ToggleActions(readerPlugged)
-{
-	$("#txtPaymentAmount").prop('disabled', !readerPlugged);
-	if (readerPlugged){
-		$("#divPay").show();
-		$("#divInsertReader").hide();
-		$("#txtPaymentAmount").focus();
-	}
-	else{
-		$("#divPay").hide();
-		$("#divInsertReader").show();
-	}
-	
-}
