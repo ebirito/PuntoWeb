f30104e38dddc7090263ca4e67b4c77bb320aee8
Abraham Lapscher <ebirito@yahoo.com>
2013-05-27 14:10:23 -0400

    Payment entry format and saving payments to backend

--- scripts/payment.js
+++ scripts/payment.js
@@ -1,4 +1,51 @@
-function btnPayClick()
+function CreateAndSavePayment()
 {
-	$.mobile.changePage('signature.html');
+	//Validate amount
+	if (_amount <= 0)
+	{
+		var validator = $("#frmPayment").validate();
+		validator.showErrors({"txtPaymentAmount": "Favor ingrese monto valido"});
+		return false;
+	}
+	
+	// Create the payment record and save in backend
+	var payment = CreatePayment();
+	payment.create({
+    success: function(model) {
+        alert("Payment saved");
+        $.mobile.changePage('signature.html');
+    },
+    error: function(model, response) {
+        alert("Error saving payment");
+    }});
+}
+
+function CreatePayment()
+{
+	// Get card info from the reader
+	var cardInfo = GetCardInfo();
+	var Payment = StackMob.Model.extend({ schemaName: 'Payment' });
+	var newPayment = new Payment(
+		{
+			Date: new Date(),
+			Amount: _amount,
+			CardHolderFirstName: cardInfo.CardHolderFirstName,
+			CardHolderMiddleInitial: cardInfo.CardHolderMiddleInitial,
+			CardHolderLastName: cardInfo.CardHolderLastName,
+			CardLastFourNumbers: cardInfo.CardLastFourNumbers
+		}
+	);
+	
+	return newPayment;
+}
+
+function GetCardInfo()
+{
+	var cardInfo = new Object();
+	cardInfo.CardHolderFirstName = "Jose";
+	cardInfo.CardHolderMiddleInitial = "M";
+	cardInfo.CardHolderLastName = "Perez";
+	cardInfo.CardLastFourNumbers = "4242";
+	
+	return cardInfo;
 }
