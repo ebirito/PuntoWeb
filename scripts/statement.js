23c0548206c28df8e029d74bbfecdbabb0212477
Boris Polania <boris.polania@gmail.com>
2013-06-05 14:13:27 -0400

    The statement has a complete new look and feel, the table titles were updated and spacing issues were fixed.
    In the menu configuration page the Punto icon was updated.
    
    The config-menu.html and the statement files were affected in this
    commit

--- scripts/statement.js
+++ scripts/statement.js
@@ -2,13 +2,13 @@
 
 // Get statement with the last X transactions from the Stackmob 'payments' schema and display in on Jquery Mobile listview
 function getPayments() {
-		
+
 	/*
 		The number of transactions displayed in the statement (numberOfRecords) must be eventually dinamically assigned from
 		a configuration interface.
 	*/ 
 	var numberOfRecords = 10;
-			
+
 	var Payment = StackMob.Model.extend({
         schemaName:'payment'
     });
@@ -21,26 +21,35 @@ function getPayments() {
 	var q = new StackMob.Collection.Query();
 	//Queries the transaction of this logged user
 	q.equals('sm_owner', 'user/'+_user.get('username'));
-	 
+
 	payments.query(q,{
     	success: function(model) {
 
 			var ps = "";
-			
+
 			//On success generates a <li> for each register
 			payments.each(function(item,key){
-			
+
 					 if (key > payments.size() - (numberOfRecords + 1)) {
-						 
+
 						 var date = new Date(item.get('date'));
-						 
-						 ps = '<li>'+item.get('cardholderfirstname')+ ' ' +item.get('cardholdermiddleinitial')+ ' ' +item.get('cardholderlastname')+ ' ************' +item.get('cardlastfournumbers')+  ' ' + accounting.formatMoney(parseFloat(item.get('amount')))+ ' ' +$.datepicker.formatDate('dd-mm-yy', date)+'</li>' + ps;
+
+						 //ps = '<li>'+item.get('cardholderfirstname')+ ' ' +item.get('cardholdermiddleinitial')+ ' ' +item.get('cardholderlastname')+ ' ************' +item.get('cardlastfournumbers')+  ' ' + accounting.formatMoney(parseFloat(item.get('amount')))+ ' ' +$.datepicker.formatDate('dd-mm-yy', date)+'</li>' + ps;
+					 
+						ps = '<tr><td>'+item.get('cardholderfirstname')+ ' ' +item.get('cardholdermiddleinitial')+ ' ' +item.get('cardholderlastname')+ '</td><td>' +item.get('cardlastfournumbers')+  '</td><td>' + accounting.formatMoney(parseFloat(item.get('amount')))+ '</td><td>'+$.datepicker.formatDate('dd-mm-yy', date)+'</td></tr>' + ps;
 					 }	 
-					
+
                });
 
+			
+			console.debug(ps);
+			var tb = $('#table-body');
+			tb.empty().append(ps);
+			$("#payments-table").trigger('create')
+			/*
 			$("#transactionMenu").append(ps);
 			$("#transactionMenu").listview("refresh");
+			*/
     	},
     	error: function(model, response) {
         	console.debug(response);
